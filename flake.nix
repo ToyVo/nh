bf935c422a372edcd51e9c9e3dca9474dd1c6f97
Collin Diekvoss <Collin@Diekvoss.com>
2025-04-15 13:51:39 -0500

    remove home manager module

--- flake.nix
+++ flake.nix
@@ -39,9 +39,6 @@
       nixDarwinModules.prebuiltin = import ./darwin-module.nix self;
       # use this module after that pr is merged
       nixDarwinModules.default = import ./module.nix self;
-      # use this module before this pr is merged https://github.com/nix-community/home-manager/pull/5304
-      homeManagerModules.prebuiltin = import ./home-manager-module.nix self;
-      # use this module after that pr is merged
       homeManagerModules.default = import ./module.nix self;
     };
 }
